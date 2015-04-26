ca2d94e9e7226d17b95a7adceb84f01ae76ea819
Matthew Steglinski <matthew.steglinski@utoronto.ca>
2015-04-26 10:56:58 -0400

    Readd the ability to display if Factions are raidable in /f show

--- src/main/java/com/massivecraft/factions/cmd/CmdShow.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdShow.java
@@ -56,6 +56,7 @@ public class CmdShow extends FCommand {
 
         double powerBoost = faction.getPowerBoost();
         String boost = (powerBoost == 0.0) ? "" : (powerBoost > 0.0 ? TL.COMMAND_SHOW_BONUS.toString() : TL.COMMAND_SHOW_PENALTY.toString() + powerBoost + ")");
+        String raidable = faction.getLandRounded() > faction.getPowerRounded() ? TL.RAIDABLE_TRUE.toString() : TL.RAIDABLE_FALSE.toString();
 
         List<FancyMessage> allies = new ArrayList<FancyMessage>();
         List<FancyMessage> enemies = new ArrayList<FancyMessage>();
@@ -148,7 +149,7 @@ public class CmdShow extends FCommand {
             return;
         }
         msg(TL.COMMAND_SHOW_JOINING.toString() + peaceStatus, (faction.getOpen() ? TL.COMMAND_SHOW_UNINVITED.toString() : TL.COMMAND_SHOW_INVITATION.toString()));
-        msg(TL.COMMAND_SHOW_POWER, faction.getLandRounded(), faction.getPowerRounded(), faction.getPowerMaxRounded(), boost);
+        msg(TL.COMMAND_SHOW_POWER, faction.getLandRounded(), faction.getPowerRounded(), faction.getPowerMaxRounded(), boost, raidable);
         if (faction.isPermanent()) {
             msg(TL.COMMAND_SHOW_PERMANENT);
         }
